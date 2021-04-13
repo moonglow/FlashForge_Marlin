7b31bdf47858a608ec748b5324866e3c8089fd49
Chandra Tungathurthi <tckb@tgrthi.me>
2021-04-13 22:35:37 +0200

    enabling more updates to LCD data
    - enabling progress updates
    - enabling scrolling statuses

--- Marlin/Marlin/Configuration_adv.h
+++ Marlin/Marlin/Configuration_adv.h
@@ -1126,7 +1126,7 @@
 #endif // HAS_LCD_MENU
 
 // Scroll a longer status message into view
-//#define STATUS_MESSAGE_SCROLLING
+#define STATUS_MESSAGE_SCROLLING
 
 // On the Info Screen, display XY with one decimal place when possible
 //#define LCD_DECIMAL_SMALL_XY
@@ -1135,10 +1135,10 @@
 #define LCD_TIMEOUT_TO_STATUS 60000
 
 // Add an 'M73' G-code to set the current percentage
-//#define LCD_SET_PROGRESS_MANUALLY
+#define LCD_SET_PROGRESS_MANUALLY
 
 // Show the E position (filament used) during printing
-//#define LCD_SHOW_E_TOTAL
+#define LCD_SHOW_E_TOTAL
 
 #if ENABLED(SHOW_BOOTSCREEN)
   #define BOOTSCREEN_TIMEOUT 1000        // (ms) Total Duration to display the boot screen(s)
