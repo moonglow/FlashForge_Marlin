a31b3502d43d42453273647b1459bd8d3face8b7
Chandra Tungathurthi <tckb@tgrthi.me>
2021-04-16 07:41:37 +0200

    updating the nozzle park position on dreamer_nx to  RIGHT_BACK

--- configs/Configuration-dreamer_nx.h
+++ configs/Configuration-dreamer_nx.h
@@ -2,4 +2,4 @@
 #define X_MAX_POS   158.50
 #define Y_MIN_POS   -76.23
 #define Y_MAX_POS    76.23
-#define NOZZLE_PARK_POINT { (X_MAX_POS - 10), (Y_MIN_POS + 10), 0 }
+#define NOZZLE_PARK_POINT { (X_MAX_POS - 10), (Y_MAX_POS - 10), 0 }
