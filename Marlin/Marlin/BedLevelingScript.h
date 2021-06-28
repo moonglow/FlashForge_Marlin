99bee0e610449af6c9a72ddf38c07a9d7be5f09a
moonglow <fxdteam@gmail.com>
2021-06-28 10:50:50 +0300

    *update bed leveling script

--- Marlin/Marlin/BedLevelingScript.h
+++ Marlin/Marlin/BedLevelingScript.h
@@ -25,7 +25,7 @@
 "G1 Z0 F420\n"\
 "M0 Press to finish\n"\
 "M117 Leveling done\n"\
-"G1 Z5 F420\n"\
+"G1 Z10 F420\n"\
 "G28 X Y\n"\
 "M84"
 #elif ENABLED( FF_INVENTOR_MACHINE )
@@ -53,7 +53,7 @@
 "G1 Z0 F420\n"\
 "M0 Press to finish\n"\
 "M117 Leveling done\n"\
-"G1 Z5 F420\n"\
+"G1 Z10 F420\n"\
 "G28 X Y\n"\
 "M84"
 #endif
@@ -74,8 +74,9 @@
 "M0 Adjust the screw and press to continue\n"\
 "G1 X0 Y0 Z10 F3000\n"\
 "G1 Z0 F800\n"\
+"M0 Press to finish\n"\
 "M117 Leveling done\n"\
-"G1 Z5 F420\n"\
+"G1 Z10 F420\n"\
 "G28 X Y\n"\
 "M84"
 #endif
