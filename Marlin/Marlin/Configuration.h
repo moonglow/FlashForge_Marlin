b4af688a9edcc6e06303e92c71ebd17af2fbf4c9
Moonglow <fxdteam@gmail.com>
2021-09-22 19:48:59 +0300

    Revert some changes

--- Marlin/Marlin/Configuration.h
+++ Marlin/Marlin/Configuration.h
@@ -55,7 +55,6 @@
 #define FF_DREAMER_NX_MACHINE
 #endif
 
-//===========================================================================
 
 /**
  * Configuration.h
@@ -1426,7 +1425,7 @@
  */
 #define Z_IDLE_HEIGHT Z_HOME_POS
 
-//#define Z_HOMING_HEIGHT  0      // (mm) Minimal Z height before homing (G28) for Z clearance above the bed, clamps, ...
+//#define Z_HOMING_HEIGHT  4      // (mm) Minimal Z height before homing (G28) for Z clearance above the bed, clamps, ...
                                   // Be sure to have this much clearance over your Z_MAX_POS to prevent grinding.
 
 //#define Z_AFTER_HOMING  10      // (mm) Height to move to after homing Z
