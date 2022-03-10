29e11989f07434a371ec6e336986a859075247ff
Roman Ilichev <fxdteam@gmail.com>
2022-03-10 18:51:46 +0300

    *update calibration script for Dremel 3D20 *dead files cleanup

--- Marlin/Marlin/BedLevelingScript.h
+++ Marlin/Marlin/BedLevelingScript.h
@@ -58,6 +58,32 @@
 "M84"
 #endif
 #else
+#if ENABLED( FF_DREMEL_3D20_MACHINE )
+  #define MAIN_MENU_ITEM_3_GCODE \
+"G90\n"\
+"M117 Bed leveling started\n"\
+"G28\n"\
+"G1 Z10 F800\n"\
+"G1 X-57.7 Y-67 F3000\n"\
+"G1 Z0 F800\n"\
+"M0 Adjust the screw and press to continue\n"\
+"G1 Z10 F800\n"\
+"G1 X41.3 Y-67 F3000\n"\
+"G1 Z0 F800\n"\
+"M0 Adjust the screw and press to continue\n"\
+"G1 Z10 F800\n"\
+"G1 X-6.6 Y60 F3000\n"\
+"G1 Z0 F800\n"\
+"M0 Adjust the screw and press to continue\n"\
+"G1 Z10 F800\n"\
+"G1 X0 Y0 F3000\n"\
+"G1 Z0 F800\n"\
+"M0 Press to finish\n"\
+"M117 Leveling done\n"\
+"G1 Z10 F800\n"\
+"G28 X Y\n"\
+"M84"
+#else
 /* original script for FlashForge NX/Dreamer/Inventor/ */
   #define MAIN_MENU_ITEM_3_GCODE \
 "G90\n"\
@@ -83,4 +109,5 @@
 "G1 Z10 F800\n"\
 "G28 X Y\n"\
 "M84"
+#endif
 #endif
